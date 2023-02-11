7fe27f7cc8473e4eb0e128bde31c721fd6e2ccd5
James McNeilis <jmcneilis@outlook.com>
2023-02-11 13:08:57 +0000

    Added test router for E2E testing (#4)

--- app.js
+++ app.js
@@ -17,6 +17,11 @@ app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
 
+if (process.env.NODE_ENV === 'test') {
+  const testsRouter = require('./controllers/tests')
+  app.use('/api/tests', testsRouter)
+}
+
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
 
